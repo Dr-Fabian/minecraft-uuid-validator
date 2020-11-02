653d60928ad76e8d69332e7830713b4d15d1f761
Dr-Fabian <fabiancostica@gmail.com>
2020-11-02 17:51:52 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @param arg
+ * @returns true if the argument is a valid UUID or false if it's not
+ */
+function validator(arg: string): boolean{
+	return /([0-9A-Z]{8})-([0-9A-Z]{4})-([0-9A-Z]{4})-([0-9A-Z]{12})/i.test(arg);
+}
+export { validator };
