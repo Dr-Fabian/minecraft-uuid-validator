2631fb24798a68836687208637370d91f38b8e41
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-02-11 23:47:42 -0500

    switch to hexadecimal

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@
  * @returns true if the argument is a valid UUID or false if it's not
  */
 function validator(arg: string): boolean{
-	return /([0-9A-Z]{8})-([0-9A-Z]{4})-([0-9A-Z]{4})-([0-9A-Z]{4})-([0-9A-Z]{12})/i.test(arg);
+	return /([0-9A-F]{8})-([0-9A-F]{4})-([0-9A-F]{4})-([0-9A-F]{4})-([0-9A-F]{12})/i.test(arg);
 }
 export { validator };
